5a091952031a5f1f8efcc97fe612063da1e65a65
paularecaj <paularecajgarcia@gmail.com>
2026-06-07 13:49:49 +0200

    Configuration for Sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,12 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.3.21" apply false
     id("org.jetbrains.kotlin.kapt") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version "2.3.21" apply false
+    id("org.sonarqube") version "7.3.1.8318"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "PaulaRecaj_LKS_Parking_PaulaRecaj")
+    property("sonar.organization", "paularecaj")
+  }
 }
